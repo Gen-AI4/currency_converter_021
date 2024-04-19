562f8464a96cfcb74fb6da2c120be34728858b8b
Ahmed Ali <160950887+Gen-AI4@users.noreply.github.com>
2024-04-19 12:45:11 +0500

    making just simple currency converter

---
+++ main.ts
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const currency: any = {
+  USD: 1,
+  PKR: 278,
+  EUR: 0.92,
+  AUD: 1.52,
+  SAR: 3.75,
+  KDN: 0.31,
+};
+let user_input = await inquirer.prompt([
+  {
+    name: "fromCurrency",
+    type: "list",
+    message: "Select your currency",
+    choices: ["USD", "PKR", "EUR", "AUD", "SAR", "KDN"],
+  },
+  {
+    name: "toCurrency",
+    type: "list",
+    message: "Enter to currency",
+    choices: ["USD", "PKR", "EUR", "AUD", "SAR", "KDN"],
+  },
+  {
+    name: "amount",
+    type: "number",
+    message: "Enter your Amount for convert",
+  },
+]);
+
+if (user_input.fromCurrency === user_input.toCurrency) {
+  console.log("Ops! you're choosing same currency");
+} else {
+  let fromCurrencyAmount = currency[user_input.fromCurrency];
+  let toCurrencyAmount = currency[user_input.toCurrency];
+  let Amount = user_input.amount;
+  let baseAmount = Amount / fromCurrencyAmount;
+  let totalConvertedAmount = baseAmount * toCurrencyAmount;
+  console.log(`Your Converted Currency Amount is:`);
+  console.log(totalConvertedAmount);
+}
